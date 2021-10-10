96326fdd34f0ca9f606fdb167207144a4846f937
huangzx <huangzhixi@youxin.cloud>
2021-10-10 23:26:06 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  /**配置环境 */
+  mode: 'development',
+  /**配置入口 */
+  entry: {
+    app: './src/index.js',
+  },
+  /**配置出口 */
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js',
+  },
+  /**配置热更新 */
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+  },
+  /**配置插件 */
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from:  'public/*.ico',
+          to: path.join(__dirname, './dist'),
+        },
+      ],
+    }),
+  ],
+};
